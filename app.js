29a4ff135861252ea8186dce56dd255b750113bd
Bernard828 <bernard.lackey1@gmail.com>
2023-02-16 12:11:16 -0500

    fixed 'lin(k) const'

---
+++ app.js
@@ -0,0 +1,41 @@
+//DEFINE UI VARS
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector(',clear-task');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+//LOAD ALL EVENT LISTENERS
+loadEventListeners();
+//load all event listenrers
+function loadEventListeners() {
+    form.addEventListener('submit', addTask);
+}
+//add task
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert('Add a task')
+    }
+
+    //create li element
+    const li = document.createElement('li');
+    //add class
+    li.className = 'collection-item';
+    //create text node and append child
+    li.appendChild(document.createTextNode(taskInput.value));
+    //create new link element
+    const link = document.createElement('a');
+    //add class
+    link.className = 'delete-item secondary-content';
+    //add icon html
+    link.innerhtml = '<i class="fa fa-remove"></i>';
+    //appemd the link to li
+    li.appendChild(link);
+
+    //append li to ul
+    taskList.appendChild(li);
+    //clear input
+    taskInput.value = '';
+    e.preventDefault();
+}
+
